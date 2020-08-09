68b47395fed937812eb755eed778bba1d5085075
Vu Do <vudhk@magik.vn>
2020-08-09 18:11:16 +0700

    fix: config app

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ import numpy as n
 import codecs
 from glob import glob
 import os
+import json
 
 root_path = os.path.dirname(os.path.abspath(__file__))
+config = {}
 
 def main():
+    global config
+    # Load config
+    with open(root_path + "/config.json", 'r') as f:
+        config = json.load(f)
+
+    # Init side bar
     st.sidebar.header("Main")
     query_summary = get_audio_summary()
     option = st.sidebar.radio("Go to", ("See Result", "See Statistic"))
@@ -34,6 +42,7 @@ def create_footer_sidebar():
 - Vu Do \n
 - Hai Ngo \n
 DeepHub team ♥️
+[Source Code](https://github.com/kudos-cmd/music-id-demo)
     """)
 
 def get_audio_summary():
@@ -51,19 +60,27 @@ def render_statistic():
 def display_matched_result(audio_container_name, has_pex_result=False):
     st.title(f"Result for {audio_container_name}")
 
+    content_server_name = config.get('content_server_name', "")
+
     if has_pex_result:
         st.header('PEX Result:')
-        components.iframe("http://localhost:8601/index_origin", height=300)
+        components.iframe(
+            f"{content_server_name}/index_origin", 
+            height=300)
     
     st.header('DeepHub Result:')
     query_audio_name = os.path.basename(list(glob(root_path + f"/assets/{audio_container_name}/query/*.mp3"))[0])
-    components.iframe(f"http://localhost:8601/query_predict?query_audio_container_name={audio_container_name}&query_audio_name={query_audio_name}", height=250)
+    components.iframe(
+        f"{content_server_name}/query_predict?query_audio_container_name={audio_container_name}&query_audio_name={query_audio_name}", 
+        height=250)
 
     asset_audio_container_paths = glob(root_path + f"/assets/{audio_container_name}/assets/*")
     st.header(f'Asset Segments ({len(asset_audio_container_paths)}):')
     for asset_audio_container_path in asset_audio_container_paths:
         asset_audio_name = os.path.basename(asset_audio_container_path)
-        components.iframe(f"http://localhost:8601/asset_predict?query_audio_container_name={audio_container_name}&asset_audio_name={asset_audio_name}", height=250)
+        components.iframe(
+            f"{content_server_name}/asset_predict?query_audio_container_name={audio_container_name}&asset_audio_name={asset_audio_name}", 
+            height=250)
 
 
 if __name__ == "__main__":
