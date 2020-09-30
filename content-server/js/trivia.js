57bcac2c56ec48ab1fe495f6945133840b9e5b21
Ubuntu <ubuntu@ip-172-26-4-187.ap-northeast-1.compute.internal>
2020-09-30 05:59:34 +0000

    fix: remove garbage code in trivia.js

--- content-server/js/trivia.js
+++ content-server/js/trivia.js
@@ -57,15 +57,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Navbar links
-    var ul = document.querySelector('.nav-pills');
-    var pills = ul.querySelectorAll('li');
-    var active = pills[0];
-    if (location.search) {
-        var first = location.search.split('&')[0];
-        var link = ul.querySelector('a[href="' + first + '"]');
-        if (link) {
-            active = link.parentNode;
-        }
-    }
-    active && active.classList.add('active');
+    // var ul = document.querySelector('.nav-pills');
+    // var pills = ul.querySelectorAll('li');
+    // var active = pills[0];
+    // if (location.search) {
+    //     var first = location.search.split('&')[0];
+    //     var link = ul.querySelector('a[href="' + first + '"]');
+    //     if (link) {
+    //         active = link.parentNode;
+    //     }
+    // }
+    // active && active.classList.add('active');
 });
